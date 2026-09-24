50e1bbb100f8aa7368e8472dbfcfb3a2540ce2c7
wuyuqq <33753437+wuyuqq@users.noreply.github.com>
2026-09-24 12:31:48 +0800

    Update ipv6-check.js

--- loon/script/ipv6-check.js
+++ loon/script/ipv6-check.js
@@ -222,7 +222,7 @@ $httpClient.get(
         $notification.post(
             "IPv6 地址发生变化",
             "",
-            time + "\n\n" +
+            time + "\n" +
             "当前 IPv6:\n" +
             currentIPv6
         );
